1d1130bca66907ae9fc2645e0ccafabd1883f2c6
jstavanja <jstavanja@gmail.com>
2016-07-15 10:32:29 +0200

    Demo private key implementation

--- server.js
+++ server.js
@@ -61,11 +61,27 @@ fs.readFile('po_parametru.html', 'utf8', function (err,data) {
   plotHtmlPotem = data;
 });
 
+// attention, very secure stuff (just for demo tho)
+var passwordi = ["medrockweek1", "record>ostali"];
+
+function isValidPass(pass, passwords) {
+    for (var i = 0; i < passwords.length; i++) {
+        if (passwords[i] == pass) return true;
+    }
+    return false;
+}
+
 streznik.post('/plot', function(req, res) {
 
     ehrId = req.body.lg_patientid;
-
-    res.send(plotHtmlPrej + ehrId + plotHtmlPotem);
+    pass = req.body.lg_password;
+    
+    if (isValidPass(pass, passwordi)) {     
+        res.send(plotHtmlPrej + ehrId + plotHtmlPotem);
+    } else {
+        
+        res.send("Your password ( "+pass+" ) is not correct.");
+    }
 
     // res.send('You sent the name "' + req.body.lg_patientid + '".');
 });
